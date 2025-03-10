7586ad4dff31519b03fa04ec12d149701d97187c
JesusEnjamio <jenjamiob@gmail.com>
2025-03-10 20:21:18 +0100

    testTranscribeDNAtoRNA failed

--- src/test/java/dev/jesus/rna_transcription/TranscriptionTest.java
+++ src/test/java/dev/jesus/rna_transcription/TranscriptionTest.java
@@ -1,4 +1,14 @@
 package dev.jesus.rna_transcription;
 
+import org.junit.jupiter.api.Test;
+ import static org.junit.jupiter.api.Assertions.*;
+
 public class TranscriptionTest {
+    @Test
+     public void testTranscribeDNAtoRNA() {
+         String dna = "ATCG";
+         String expectedRNA = "UAGC";
+         String actualRNA = Transcription.transcribeDNAtoRNA(dna);
+         assertEquals(expectedRNA, actualRNA, "The DNA to RNA transcription failed.");
+     }
 }
